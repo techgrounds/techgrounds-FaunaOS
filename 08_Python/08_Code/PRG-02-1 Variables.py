4602644c79031a2de47db960491abc461cf6dcc2
Replit user <jordan.hodge.lifeos@gmail.com>
2023-08-14 11:20:19 +0200

    Update

--- 08_Python/08_Code/PRG-02-1 Variables.py
+++ 08_Python/08_Code/PRG-02-1 Variables.py
@@ -1,10 +1,18 @@
+#Create a new script.
 
+#Create two variables x and y. Assign a numerical value to both variables.
 x = 10
-y = 13
+y = 13 
 
+#Print the values of x and y.
+print (x,y)  
+
+#Create a third variable named z. The value of z should be the sum of x and y.
 z = x + y
 
+#Print the value of z. 
 print("z =", z)
+
 
 
 
