7cf47c6a14a8fa9aacd2aad267d64420a0729143
Replit user <jordan.hodge.lifeos@gmail.com>
2023-08-14 15:14:01 +0200

    update

--- 08_Python/08_Code/PRG-02-3 Variables.py
+++ 08_Python/08_Code/PRG-02-3 Variables.py
@@ -1,6 +1,4 @@
 #Create a new script.
-# ✅
-
 #Create a variable and assign a value to it.
 var_1 = "Jordan"
 
@@ -18,6 +16,3 @@ var_3 = "Student"
 
 #Print the text “Value 3: <value 3 here>”.
 print(var_3)
-
-#Print all 3
-print(var_1,var_2,var_3)
