f4f4916ea932b04fe1b521948a373f60f6013428
Replit user <jordan.hodge.lifeos@gmail.com>
2023-08-15 10:36:29 +0200

    Update

---
+++ 08_Python/08_Code/PRG-03.py
@@ -0,0 +1,15 @@
+#Create a new script.
+#Copy the code below into your script.
+a = 'int'
+b = 7
+c = False
+d = "18.5"
+#Determine the data types of all four variables (a, b, c, d) using a built in function.
+Type 'a': <class 'str'>
+Type 'b': <class 'int'>
+Type 'c': <class 'bool'>
+Type 'd': <class 'str'>
+#Make a new variable x and give it the value b + d. Print the value of x. This will raise an error. Fix it so that print(x) prints a float.
+
+#Write a comment above every line of code that tells the reader what is going on in your script.
+
